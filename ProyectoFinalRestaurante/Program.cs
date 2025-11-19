a4ed6efbde3367bb1c4c83bb64053277bdac3af9
CHuCH0o <jjgutierrez113077@umanizales.edu.co>
2025-11-19 18:22:50 -0500

    Program.cs

--- ProyectoFinalRestaurante/Program.cs
+++ ProyectoFinalRestaurante/Program.cs
@@ -1,3 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace ProyectoFinalRestaurante
+{
+    // aqui empieza el sistema del restaurante
+    // este archivo solo muestra el menu principal
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // aqui creo la lista donde se guardan todos los restaurantes
+            ListaEnlazada<Restaurante> restaurantes = new ListaEnlazada<Restaurante>();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("--- SISTEMA RESTAURANTE ---");
+                Console.WriteLine("1. Restaurantes");
+                Console.WriteLine("2. Clientes");
+                Console.WriteLine("3. Platos");
+                Console.WriteLine("4. Pedidos");
+                Console.WriteLine("5. Reportes");
+                Console.WriteLine("99. Salir");
+                Console.Write("Escriba una opcion: ");
+                string opcion = Console.ReadLine();
+
+                if (opcion == "99")
+                {
+                    // aqui termino el programa
+                    return;
+                }
+                else if (opcion == "1")
+                {
+                    Restaurante.MenuRestaurantes(restaurantes);
+                }
+                else if (opcion == "2")
+                {
+                    Cliente.MenuClientes(restaurantes);
+                }
+                else if (opcion == "3")
+                {
+                    Plato.MenuPlatos(restaurantes);
+                }
+                else if (opcion == "4")
+                {
+                    Pedido.MenuPedidos(restaurantes);
+                }
+                else if (opcion == "5")
+                {
+                    Pedido.MenuReportes(restaurantes);
+                }
+                else
+                {
+                    Console.WriteLine("opcion no valida");
+                    Console.WriteLine("enter para seguir");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
